edf60d91cbc8ab1340c3e49580090cd91daa6641
pstaten <paulstaten@hotmail.com>
2026-01-05 12:14:59 -0500

    fixed typo

--- exp/MiMA/MiMA_hiheat0p1_qbo20.py
+++ exp/MiMA/MiMA_hiheat0p1_qbo20.py
@@ -83,7 +83,7 @@ diag.add_field('atmosphere', 'rh', time_avg=True)
 
 # === HS forcing diagnostics ===
 diag.add_field('hs_forcing', 'teq', time_avg=True)
-diag.add_field('hs_forcing', 'tdt_ndamp', time_avg=True
+diag.add_field('hs_forcing', 'tdt_ndamp', time_avg=True)
 
 # === Damping driver diagnostics (QBO and EWA heating) ===
 diag.add_field('damping', 'udt_qbo', time_avg=True)
